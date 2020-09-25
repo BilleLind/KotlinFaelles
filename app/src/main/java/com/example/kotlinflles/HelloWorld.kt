21c70c974276b380ebc598deb90747bcf7d380ac
yokarak <54890038+yokarak@users.noreply.github.com>
2020-09-25 09:14:39 +0200

    added activity_hello_world.xml made a button to continue to the new activity

---
+++ app/src/main/java/com/example/kotlinflles/HelloWorld.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinflles
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HelloWorld : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello_world)
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/kotlinflles/MainActivity.kt
+++ app/src/main/java/com/example/kotlinflles/MainActivity.kt
@@ -1,22 +1,42 @@
 package com.example.kotlinflles
 
+import android.app.Application
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import org.w3c.dom.Text
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var county=1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var county=1
         val increase = findViewById<Button>(R.id.increase)
-        val count = findViewById<TextView>(R.id.count)
+        val count: TextView = findViewById(R.id.count)
+        val videre: Button = findViewById(R.id.videre)
 
         increase.setOnClickListener {
-            county +=1
-            count.text = county.toString()
+            change()
+            val temp:String = county.toString()
+            count.setText(temp)
+            Toast.makeText(applicationContext, hentSvar(), Toast.LENGTH_SHORT).show()
         }
+        videre.setOnClickListener {
+            startActivity(Intent(this, HelloWorld::class.java))
+        }
+    }
+    fun change() {
+        county += 1
+    }
+    fun hentSvar() : String {
+        val svar = when {
+            county ==5 -> "så nåede vi 5"
+            county <6 -> "så er det bare fremad"
+            county == 10 -> "yeah 10"
+            else -> "mon jeg virker" }
+        return svar
     }
 }
