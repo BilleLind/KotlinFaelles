76bdb9e3455643fce2467d745053c6428019f245
KRL1991 <kimx4706@edu.easj.dk>
2020-09-25 11:06:14 +0200

    Added image buttons to Companies

--- app/src/main/java/com/example/kotlinflles/Companies.kt
+++ app/src/main/java/com/example/kotlinflles/Companies.kt
@@ -2,10 +2,25 @@ package com.example.kotlinflles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_companies.*
 
 class Companies : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_companies)
+
+
+        button
+
+        button2
+
+        imageButton2
+
+        imageButton3
+
+        imageButton4
+
+        imageButton5
     }
 }
