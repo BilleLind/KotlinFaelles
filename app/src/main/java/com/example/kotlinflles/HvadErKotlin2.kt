12d7a65cb15a1a9a5660cb5ea2c965f2300baacc
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-09-25 11:19:12 +0200

    Detter er mit commit dog uden buttons

---
+++ app/src/main/java/com/example/kotlinflles/HvadErKotlin2.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinflles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HvadErKotlin2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hvad_er_kotlin2)
+    }
+}
---
+++ app/src/main/java/com/example/kotlinflles/Hvad_er_Kotlin.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinflles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RelativeLayout
+
+class Hvad_er_Kotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hvad_er__kotlin)
+
+
+
+    }
+}
